71c437c2cd9e6ed34fe576028a7707ad42651532
jasper_wu <wuyanghui1997@gmail.com>
2025-12-22 23:43:09 +0800

    fix mongo db to python 3.12

--- utility/property_listing_init.py
+++ utility/property_listing_init.py
@@ -3,21 +3,9 @@ import os
 import sys
 from importlib import util as importlib_util
 from urllib.parse import quote
-
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
-
-logger = logging.getLogger(__name__)
-if not logger.handlers:
-    logging.basicConfig(level=logging.INFO)
-
-try:
-    from pymongo.mongo_client import MongoClient
-except ModuleNotFoundError as exc:
-    logger.error("Failed to import pymongo.mongo_client: %s", exc)
-    logger.info("Python executable: %s", sys.executable)
-    logger.info("sys.path entries: %s", sys.path)
-    logger.info("importlib.util.find_spec('pymongo') returned: %s", importlib_util.find_spec("pymongo"))
-    raise
 
 load_dotenv()
 db_password = os.environ.get("MONGODB_PW")
@@ -25,10 +13,8 @@ encoded_pw = quote(db_password, safe='')
 
 if db_password:
     # Use an f-string to safely insert the password into the URI
-    uri = f"mongodb+srv://Vercel-Admin-property_listing:{encoded_pw}@cluster0.fznmnjx.mongodb.net/?appName=Cluster0"    
-    # Create a new client and connect to the server
-    client = MongoClient(uri)
-
+    uri = f"mongodb+srv://Vercel-Admin-property_listing:{encoded_pw}@cluster0.fznmnjx.mongodb.net/?appName=Cluster0"    # Create a new client and connect to the server
+    client = MongoClient(uri, server_api=ServerApi('1'))
     # Send a ping to confirm a successful connection
     try:
         client.admin.command('ping')
