48b40744f23c1e6421d81806364e4c1fb39c68d6
jasper_wu <wuyanghui1997@gmail.com>
2026-03-19 23:19:20 +0800

    model update

--- utility/llm_init.py
+++ utility/llm_init.py
@@ -2,24 +2,18 @@ import os
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 import os
-
+from openai import OpenAI
 load_dotenv()
-
+client = OpenAI()
 openai_api_key = os.getenv("OPENAI_API_KEY", "").strip()
 
-def load_llm(model = 'gpt-4.1'):
+def load_llm(model = "openai/gpt-5.4-mini"):
     llm = ChatOpenAI(
-        model=model,
-        api_key=openai_api_key
-        # stream_usage=True,
-        # temperature=None,
-        # max_tokens=None,
-        # timeout=None,
-        # reasoning_effort="low",
-        # max_retries=2,
-        # api_key="...",  # If you prefer to pass api key in directly
-        # base_url="...",
-        # organization="...",
-        # other params...
+        api_key=os.getenv("AI_GATEWAY_API_KEY"),
+        base_url="https://ai-gateway.vercel.sh/v1"
     )
     return llm
+
+def get_embedding(text, model="text-embedding-3-small"):
+    text = text.replace("\n", " ")
+    return client.embeddings.create(input = [text], model=model).data[0].embedding
